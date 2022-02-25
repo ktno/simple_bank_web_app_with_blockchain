5781febf9e6aff5e5457ab588e7435a93b054509
ktno <kittinan@atato.com>
2022-02-26 02:36:39 +0700

    update contract addresses

--- react/src/config/constants/simpleBank.ts
+++ react/src/config/constants/simpleBank.ts
@@ -4,7 +4,7 @@ import { Bank } from './types'
 const bank: Bank = {
   token: tokens.simpleToken,
   contractAddress: {
-    97: '0xb5a1e45fA3e50D1C9d91d3d5A90aCB62D4acC314',
+    97: '0x07CEbC7Fd86Fffcb3557852Cc3B764FAa1b3A000',
     1337: process.env.REACT_APP_LOCAL_SIMPLE_BANK_CONTRACT_ADDRESS,
   },
 }
--- react/src/config/constants/tokens.ts
+++ react/src/config/constants/tokens.ts
@@ -5,7 +5,7 @@ const tokens: TokenList = {
     symbol: 'ST',
     decimals: 18,
     address: {
-      97: '',
+      97: '0xD05C072B71687514DeBaf7c8282447377436b433',
       1337: process.env.REACT_APP_LOCAL_SIMPLE_TOKEN_ADDRESS,
     },
   },
